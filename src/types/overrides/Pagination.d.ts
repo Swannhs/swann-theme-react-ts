ea2517807cdeb756d6d718faa8b05af23745444e
swann <swann@fortunatis.ch>
2023-10-10 01:16:56 +0600

    Initialize commit

---
+++ src/types/overrides/Pagination.d.ts
@@ -0,0 +1,16 @@
+// eslint-disable-next-line
+import * as Pagination from '@mui/material/Pagination';
+
+declare module '@mui/material/Pagination' {
+  interface PaginationPropsColorOverrides {
+    error;
+    success;
+    warning;
+    info;
+  }
+
+  interface PaginationPropsVariantOverrides {
+    contained;
+    combined;
+  }
+}
